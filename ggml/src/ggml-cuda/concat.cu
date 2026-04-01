73e64ac7a1d1f4084fe27a8142cf433f24df4793
Nikodem Eluszkiewicz <elusznik@gmail.com>
2026-04-01 23:28:29 +0200

    ggml/cuda: add F16 support to concat operation
    Templatize concat kernels to support both F32 and F16 types,
    enabling F16 concat for contiguous and non-contiguous paths.

--- ggml/src/ggml-cuda/concat.cu
+++ ggml/src/ggml-cuda/concat.cu
@@ -1,7 +1,8 @@
 #include "concat.cuh"
 
 // contiguous kernels
-static __global__ void concat_f32_dim0(const float * x, const float * y, float * dst, const int ne0, const int ne00) {
+template <typename T>
+static __global__ void concat_dim0(const T * x, const T * y, T * dst, const int ne0, const int ne00) {
     int nidx = threadIdx.x + blockIdx.x * blockDim.x;
     if (nidx >= ne0) {
         return;
@@ -27,7 +28,8 @@ static __global__ void concat_f32_dim0(const float * x, const float * y, float *
     }
 }
 
-static __global__ void concat_f32_dim1(const float * x, const float * y, float * dst, const int ne0, const int ne01) {
+template <typename T>
+static __global__ void concat_dim1(const T * x, const T * y, T * dst, const int ne0, const int ne01) {
     int nidx = threadIdx.x + blockIdx.x * blockDim.x;
     if (nidx >= ne0) {
         return;
@@ -53,7 +55,8 @@ static __global__ void concat_f32_dim1(const float * x, const float * y, float *
     }
 }
 
-static __global__ void concat_f32_dim2(const float * x, const float * y, float * dst, const int ne0, const int ne02) {
+template <typename T>
+static __global__ void concat_dim2(const T * x, const T * y, T * dst, const int ne0, const int ne02) {
     int nidx = threadIdx.x + blockIdx.x * blockDim.x;
     if (nidx >= ne0) {
         return;
@@ -79,24 +82,25 @@ static __global__ void concat_f32_dim2(const float * x, const float * y, float *
     }
 }
 
-static void concat_f32_cuda(const float * x, const float * y, float * dst, int ne00, int ne01, int ne02, int ne0, int ne1, int ne2, int dim, cudaStream_t stream) {
+template <typename T>
+static void concat_cuda(const T * x, const T * y, T * dst, int ne00, int ne01, int ne02, int ne0, int ne1, int ne2, int dim, cudaStream_t stream) {
     int num_blocks = (ne0 + CUDA_CONCAT_BLOCK_SIZE - 1) / CUDA_CONCAT_BLOCK_SIZE;
     dim3 gridDim(num_blocks, ne1, ne2);
     if (dim == 0) {
-        concat_f32_dim0<<<gridDim, CUDA_CONCAT_BLOCK_SIZE, 0, stream>>>(x, y, dst, ne0, ne00);
+        concat_dim0<<<gridDim, CUDA_CONCAT_BLOCK_SIZE, 0, stream>>>(x, y, dst, ne0, ne00);
         return;
     }
     if (dim == 1) {
-        concat_f32_dim1<<<gridDim, CUDA_CONCAT_BLOCK_SIZE, 0, stream>>>(x, y, dst, ne0, ne01);
+        concat_dim1<<<gridDim, CUDA_CONCAT_BLOCK_SIZE, 0, stream>>>(x, y, dst, ne0, ne01);
         return;
     }
-    concat_f32_dim2<<<gridDim, CUDA_CONCAT_BLOCK_SIZE, 0, stream>>>(x, y, dst, ne0, ne02);
+    concat_dim2<<<gridDim, CUDA_CONCAT_BLOCK_SIZE, 0, stream>>>(x, y, dst, ne0, ne02);
 }
 
 // non-contiguous kernel (slow)
-template <int dim>
+template <int dim, typename T>
 static __global__ void __launch_bounds__(CUDA_CONCAT_BLOCK_SIZE)
-    concat_f32_non_cont(
+    concat_non_cont(
         const char * src0,
         const char * src1,
               char * dst,
@@ -130,24 +134,24 @@ static __global__ void __launch_bounds__(CUDA_CONCAT_BLOCK_SIZE)
     const int64_t i2 = blockIdx.y;
     const int64_t i1 = blockIdx.x;
 
-    const float * x;
+    const T * x;
 
     for (int64_t i0 = threadIdx.x; i0 < ne0; i0 += blockDim.x) {
         if (i0 < ne00 && i1 < ne01 && i2 < ne02 && i3 < ne03) {
-            x = (const float *)(src0 + (i3       )*nb03 + (i2       )*nb02 + (i1       )*nb01 + (i0       )*nb00);
+            x = (const T *)(src0 + (i3       )*nb03 + (i2       )*nb02 + (i1       )*nb01 + (i0       )*nb00);
         } else {
             if constexpr (dim == 0) {
-                x = (const float *) (src1 + i3 * nb13 + i2 * nb12 + i1 * nb11 + (i0 - ne00) * nb10);
+                x = (const T *) (src1 + i3 * nb13 + i2 * nb12 + i1 * nb11 + (i0 - ne00) * nb10);
             } else if constexpr (dim == 1) {
-                x = (const float *) (src1 + i3 * nb13 + i2 * nb12 + (i1 - ne01) * nb11 + i0 * nb10);
+                x = (const T *) (src1 + i3 * nb13 + i2 * nb12 + (i1 - ne01) * nb11 + i0 * nb10);
             } else if constexpr (dim == 2) {
-                x = (const float *) (src1 + i3 * nb13 + (i2 - ne02) * nb12 + i1 * nb11 + i0 * nb10);
+                x = (const T *) (src1 + i3 * nb13 + (i2 - ne02) * nb12 + i1 * nb11 + i0 * nb10);
             } else if constexpr (dim == 3) {
-                x = (const float *) (src1 + (i3 - ne03) * nb13 + i2 * nb12 + i1 * nb11 + i0 * nb10);
+                x = (const T *) (src1 + (i3 - ne03) * nb13 + i2 * nb12 + i1 * nb11 + i0 * nb10);
             }
         }
 
-        float * y = (float *)(dst + i3*nb3 + i2*nb2 + i1*nb1 + i0*nb0);
+        T * y = (T *)(dst + i3*nb3 + i2*nb2 + i1*nb1 + i0*nb0);
 
         *y = *x;
     }
@@ -162,60 +166,113 @@ void ggml_cuda_op_concat(ggml_backend_cuda_context & ctx, ggml_tensor * dst) {
 
     const int32_t dim = ((int32_t *) dst->op_params)[0];
 
-    GGML_ASSERT(src0->type == GGML_TYPE_F32);
-    GGML_ASSERT(src1->type == GGML_TYPE_F32);
-    GGML_ASSERT(dst->type  == GGML_TYPE_F32);
+    const bool is_f32 = src0->type == GGML_TYPE_F32 && src1->type == GGML_TYPE_F32 && dst->type == GGML_TYPE_F32;
+    const bool is_f16 = src0->type == GGML_TYPE_F16 && src1->type == GGML_TYPE_F16 && dst->type == GGML_TYPE_F16;
+
+    GGML_ASSERT(is_f32 || is_f16);
 
     if (ggml_is_contiguous(src0) && ggml_is_contiguous(src1)) {
-        const float * src0_d = (const float *)src0->data;
-        const float * src1_d = (const float *)src1->data;
+        if (is_f32) {
+            const float * src0_d = (const float *)src0->data;
+            const float * src1_d = (const float *)src1->data;
+            float * dst_d = (float *)dst->data;
 
-        float * dst_d = (float *)dst->data;
+            if (dim != 3) {
+                for (int i3 = 0; i3 < dst->ne[3]; i3++) {
+                    concat_cuda(
+                            src0_d + i3 * (src0->nb[3] / 4),
+                            src1_d + i3 * (src1->nb[3] / 4),
+                            dst_d + i3 * ( dst->nb[3] / 4),
+                            src0->ne[0], src0->ne[1], src0->ne[2],
+                            dst->ne[0],  dst->ne[1],  dst->ne[2], dim, stream);
+                }
+            } else {
+                const size_t size0 = ggml_nbytes(src0);
+                const size_t size1 = ggml_nbytes(src1);
 
-        if (dim != 3) {
-            for (int i3 = 0; i3 < dst->ne[3]; i3++) {
-                concat_f32_cuda(
-                        src0_d + i3 * (src0->nb[3] / 4),
-                        src1_d + i3 * (src1->nb[3] / 4),
-                        dst_d + i3 * ( dst->nb[3] / 4),
-                        src0->ne[0], src0->ne[1], src0->ne[2],
-                        dst->ne[0],  dst->ne[1],  dst->ne[2], dim, stream);
+                CUDA_CHECK(cudaMemcpyAsync(dst_d,           src0_d, size0, cudaMemcpyDeviceToDevice, stream));
+                CUDA_CHECK(cudaMemcpyAsync(dst_d + size0/sizeof(float), src1_d, size1, cudaMemcpyDeviceToDevice, stream));
             }
         } else {
-            const size_t size0 = ggml_nbytes(src0);
-            const size_t size1 = ggml_nbytes(src1);
+            const half * src0_d = (const half *)src0->data;
+            const half * src1_d = (const half *)src1->data;
+            half * dst_d = (half *)dst->data;
 
-            CUDA_CHECK(cudaMemcpyAsync(dst_d,           src0_d, size0, cudaMemcpyDeviceToDevice, stream));
-            CUDA_CHECK(cudaMemcpyAsync(dst_d + size0/4, src1_d, size1, cudaMemcpyDeviceToDevice, stream));
+            if (dim != 3) {
+                for (int i3 = 0; i3 < dst->ne[3]; i3++) {
+                    concat_cuda(
+                            src0_d + i3 * (src0->nb[3] / 2),
+                            src1_d + i3 * (src1->nb[3] / 2),
+                            dst_d + i3 * ( dst->nb[3] / 2),
+                            src0->ne[0], src0->ne[1], src0->ne[2],
+                            dst->ne[0],  dst->ne[1],  dst->ne[2], dim, stream);
+                }
+            } else {
+                const size_t size0 = ggml_nbytes(src0);
+                const size_t size1 = ggml_nbytes(src1);
+
+                CUDA_CHECK(cudaMemcpyAsync(dst_d,                    src0_d, size0, cudaMemcpyDeviceToDevice, stream));
+                CUDA_CHECK(cudaMemcpyAsync(dst_d + size0/sizeof(half), src1_d, size1, cudaMemcpyDeviceToDevice, stream));
+            }
         }
     } else {
         dim3 grid_dim(dst->ne[1], dst->ne[2], dst->ne[3]);
-        auto launch_kernel = [&](auto dim) {
-            concat_f32_non_cont<dim><<<grid_dim, CUDA_CONCAT_BLOCK_SIZE, 0, stream>>>(
-                (const char *) src0->data, (const char *) src1->data, (char *) dst->data,
-                src0->ne[0], src0->ne[1], src0->ne[2], src0->ne[3],
-                src0->nb[0], src0->nb[1], src0->nb[2], src0->nb[3],
-                src1->ne[0], src1->ne[1], src1->ne[2], src1->ne[3],
-                src1->nb[0], src1->nb[1], src1->nb[2], src1->nb[3],
-                dst->ne[0], dst->ne[1], dst->ne[2], dst->ne[3],
-                dst->nb[0], dst->nb[1], dst->nb[2], dst->nb[3]);
-        };
-        switch (dim) {
-            case 0:
-                launch_kernel(std::integral_constant<int, 0>{});
-                break;
-            case 1:
-                launch_kernel(std::integral_constant<int, 1>{});
-                break;
-            case 2:
-                launch_kernel(std::integral_constant<int, 2>{});
-                break;
-            case 3:
-                launch_kernel(std::integral_constant<int, 3>{});
-                break;
-            default:
-                GGML_ABORT("Invalid dim: %d", dim);
-                break;
+        if (is_f32) {
+            auto launch_kernel = [&](auto dim) {
+                concat_non_cont<dim, float><<<grid_dim, CUDA_CONCAT_BLOCK_SIZE, 0, stream>>>(
+                    (const char *) src0->data, (const char *) src1->data, (char *) dst->data,
+                    src0->ne[0], src0->ne[1], src0->ne[2], src0->ne[3],
+                    src0->nb[0], src0->nb[1], src0->nb[2], src0->nb[3],
+                    src1->ne[0], src1->ne[1], src1->ne[2], src1->ne[3],
+                    src1->nb[0], src1->nb[1], src1->nb[2], src1->nb[3],
+                    dst->ne[0], dst->ne[1], dst->ne[2], dst->ne[3],
+                    dst->nb[0], dst->nb[1], dst->nb[2], dst->nb[3]);
+            };
+            switch (dim) {
+                case 0:
+                    launch_kernel(std::integral_constant<int, 0>{});
+                    break;
+                case 1:
+                    launch_kernel(std::integral_constant<int, 1>{});
+                    break;
+                case 2:
+                    launch_kernel(std::integral_constant<int, 2>{});
+                    break;
+                case 3:
+                    launch_kernel(std::integral_constant<int, 3>{});
+                    break;
+                default:
+                    GGML_ABORT("Invalid dim: %d", dim);
+                    break;
+            }
+        } else {
+            auto launch_kernel = [&](auto dim) {
+                concat_non_cont<dim, half><<<grid_dim, CUDA_CONCAT_BLOCK_SIZE, 0, stream>>>(
+                    (const char *) src0->data, (const char *) src1->data, (char *) dst->data,
+                    src0->ne[0], src0->ne[1], src0->ne[2], src0->ne[3],
+                    src0->nb[0], src0->nb[1], src0->nb[2], src0->nb[3],
+                    src1->ne[0], src1->ne[1], src1->ne[2], src1->ne[3],
+                    src1->nb[0], src1->nb[1], src1->nb[2], src1->nb[3],
+                    dst->ne[0], dst->ne[1], dst->ne[2], dst->ne[3],
+                    dst->nb[0], dst->nb[1], dst->nb[2], dst->nb[3]);
+            };
+            switch (dim) {
+                case 0:
+                    launch_kernel(std::integral_constant<int, 0>{});
+                    break;
+                case 1:
+                    launch_kernel(std::integral_constant<int, 1>{});
+                    break;
+                case 2:
+                    launch_kernel(std::integral_constant<int, 2>{});
+                    break;
+                case 3:
+                    launch_kernel(std::integral_constant<int, 3>{});
+                    break;
+                default:
+                    GGML_ABORT("Invalid dim: %d", dim);
+                    break;
+            }
         }
     }
 }
